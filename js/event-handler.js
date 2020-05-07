25013d5089cc59b612c1c58dd54ed87117c5dc52
Emanuel Estrada Larios <emamex98@gmail.com>
2020-05-07 18:15:03 -0500

    Fixed floor view

--- js/event-handler.js
+++ js/event-handler.js
@@ -4,9 +4,13 @@ function toolsEvent(evt)
     // GEOMETRY
 
     if (evt == 1) {
-      var geometry = new THREE.BoxGeometry();        
+
+      // CUBE
+      var geometry = new THREE.BoxGeometry();   
+
       // MATERIAL
       var material = new THREE.MeshNormalMaterial();  
+
       // MESH (GEOMETRY + MATERIAL)
       mesh = new THREE.Mesh(geometry, material);
       scene.add(mesh);
@@ -19,7 +23,7 @@ function toolsEvent(evt)
       var planeGeometry = new THREE.PlaneGeometry(10, 10, 10, 10);
       var planeMaterial = new THREE.MeshBasicMaterial({color: "grey", wireframe: true});
       var plane = new THREE.Mesh(planeGeometry, planeMaterial);
-      plane.rotation.x = -0.5 * Math.PI;
+      plane.rotation.x = -1.3;// * Math.PI;
       //plane.rotation.y = -0.3 * Math.PI;
 
       scene.add(plane);
@@ -39,8 +43,9 @@ function toolsEvent(evt)
       sceneReady = true;
     }
     
-    
     else if (evt == 7) {
+
+      // CYLINDER
       var geometry = new THREE.CylinderGeometry( 0.5, 0.5, 1,32);
       var material = new THREE.MeshNormalMaterial();
 
@@ -49,11 +54,16 @@ function toolsEvent(evt)
       scene.add(mesh);
       sceneReady = true;
     }
+
     else if (evt == 9) {
-      var geometry = new THREE.SphereGeometry(0.5, 50, 50);        
+
+      //SPHERE
+      var geometry = new THREE.SphereGeometry(0.5, 50, 50);
+
       // MATERIAL
       var material = new THREE.MeshNormalMaterial(); 
       mesh = new THREE.Mesh(geometry, material);
+
       scene.add(mesh); 
       sceneReady = true;
     }
