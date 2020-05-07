d3a911ad69e5d26274561e7434ec4a93a835a97f
Emanuel Estrada Larios <emamex98@gmail.com>
2020-05-07 14:32:04 -0500

    Added cone & pyramid

--- js/event-handler.js
+++ js/event-handler.js
@@ -15,8 +15,6 @@ function toolsEvent(evt)
     
     else if (evt == 3) {
 
-      console.log("Plane")
-
       // PLANE
       var planeGeometry = new THREE.PlaneGeometry(10, 10, 10, 10);
       var planeMaterial = new THREE.MeshBasicMaterial({color: "grey", wireframe: true});
@@ -25,20 +23,40 @@ function toolsEvent(evt)
       //plane.rotation.y = -0.3 * Math.PI;
 
       scene.add(plane);
-
-      // var geometry = new THREE.SphereGeometry(0.5, 50, 50);        
-      // // MATERIAL
-      // var material = new THREE.MeshNormalMaterial(); 
-      // mesh = new THREE.Mesh(geometry, material);
-      // scene.add(mesh); 
-       sceneReady = true;
+      sceneReady = true;
     } 
+
+    else if (evt == 5) {
+
+      // CONE
+      var geometry = new THREE.ConeGeometry(1, 2, 50);
+
+      // MATERIAL
+      var material = new THREE.MeshNormalMaterial(); 
+      mesh = new THREE.Mesh(geometry, material);
+
+      scene.add(mesh); 
+      sceneReady = true;
+    }
     
     else if (evt == 9) {
       var geometry = new THREE.SphereGeometry(0.5, 50, 50);        
       // MATERIAL
       var material = new THREE.MeshNormalMaterial(); 
       mesh = new THREE.Mesh(geometry, material);
+      scene.add(mesh); 
+      sceneReady = true;
+    }
+
+    else if (evt == 11) {
+
+      // PYRAMID
+      var geometry = new THREE.ConeGeometry(1.5, 2, 3);     
+
+      // MATERIAL
+      var material = new THREE.MeshNormalMaterial(); 
+      mesh = new THREE.Mesh(geometry, material);
+
       scene.add(mesh); 
       sceneReady = true;
     }
